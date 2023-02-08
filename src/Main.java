2c032bbd5931d8ef58b1eda683d0dae8692e2edb
Jan Michal <jan.michal@email.cz>
2023-02-08 13:47:54 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //definice promennych
+        String name;
+        String surname;
+        String dateOfBirth;
+        int numberOfSignedContracts;
+        double soldedAmount;
+        String town;
+        String licencePlate;
+        double consumption;
+        String ipAddress;
+
+
+        System.out.println("soldedAmount/numberOfSignedContracts");
+    }
+}
